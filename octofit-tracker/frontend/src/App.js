bfea1c0e636475638b7f0bffb4c573ffd601430c
stuparr <parr.stuart@gmail.com>
2025-10-01 13:52:32 +0000

    Fix: Remove duplicate <Router> from App.js, use only one BrowserRouter in index.js

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Activities from './components/Activities';
 import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
@@ -9,7 +9,7 @@ import Workouts from './components/Workouts';
 
 function App() {
   return (
-    <Router>
+    <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
@@ -34,7 +34,7 @@ function App() {
           <Route path="/" element={<h2>Welcome to OctoFit Tracker!</h2>} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 }
 
